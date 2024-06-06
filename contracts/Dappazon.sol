95a75b576dda0db1a53a1a31e33f020d5f2f669d
Vyom Raturi <vyomraturi2004@gmail.com>
2024-06-06 14:40:17 +0530

    list struct

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) {}
+}
